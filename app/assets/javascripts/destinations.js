d76e72e979d08db6780de9f0aeebff3a982c4bc5
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-31 00:01:33 -0400

    Added index controller

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -14,17 +14,27 @@
   .config([
     "$stateProvider",
     RouterFunction
+  ])
+  .controller("indexCtrl", [
+    indexCtrlFunction
   ]);
 
   function RouterFunction($stateProvider){
     $stateProvider
     .state("index", {
       url: "/",
-      templateUrl: "ng-views/destination.index.html"
+      templateUrl: "ng-views/destination.index.html",
+      controller: "indexCtrl",
+      controllerAs: "indexVM"
     })
     .state("show", {
       url: "/:id"
     });
+  }
+
+  function indexCtrlFunction(){
+    var indexVM = this;
+    indexVM.foo = "bar";
   }
 
 })();
