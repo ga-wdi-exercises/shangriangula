b2799979feb3b9048272308c48a17eb7e3bcc689
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-30 23:56:21 -0400

    Added inital routes

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -6,4 +6,25 @@
 
 (function(){
 
+  angular
+  .module("shangriangula", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("index", {
+      url: "/",
+      templateUrl: "ng-views/destination.index.html"
+    })
+    .state("show", {
+      url: "/:id"
+    });
+  }
+
 })();
